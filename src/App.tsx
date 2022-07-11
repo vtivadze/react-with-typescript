c5f00e7f4e3dd84287a4a744598acb4d68a69652
Vakhtang Tivadze <vaxotivadze@gmail.com>
2022-07-12 03:48:33 +0400

    Cross component communication

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,14 @@ import NewTodo from './components/NewTodo';
 
 function App() {
   const todos = [{id: 't1', text: 'Finish the course'}];
+
+  const todoAddHandler = (text: string) => {
+    console.log(text);
+  };
+
   return (
     <div className="App">
-      <NewTodo />
+      <NewTodo onAddTodo={todoAddHandler} />
       <TodoList items={todos} />      
     </div>
   );
