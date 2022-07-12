4151abe80a67ab4680c8b2d8812d9f0e60bf5695
Vakhtang Tivadze <vaxotivadze@gmail.com>
2022-07-12 04:13:54 +0400

    Managing state better

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,16 @@ function App() {
   const [todos, setTodos] = useState<Todo[]>([]);
 
   const todoAddHandler = (text: string) => {
-    setTodos([{id: Math.random().toString(), text: text}]);
+    setTodos(prevTodos => [
+      ...prevTodos ,
+      { id: Math.random().toString(), text: text },
+    ]);
   };
 
   return (
-    <div className="App">
+    <div className='App'>
       <NewTodo onAddTodo={todoAddHandler} />
-      <TodoList items={todos} />      
+      <TodoList items={todos} />
     </div>
   );
 }
